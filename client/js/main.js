b3dc8159d1a1b6ed0a17915715f5090a9c23e196
ahmednajm1 <najmmediaa@gmail.com>
2026-05-17 06:18:34 +0400

    feat(ui): update welcome text layout, move capture frame button, regenerate SFX catalog to 248 items, bump v1.1.3

--- client/js/main.js
+++ client/js/main.js
@@ -8,7 +8,7 @@ window.onerror = function(msg, url, line) {
     return true;
 };
 
-var CURRENT_VERSION = "1.1.2";
+var CURRENT_VERSION = "1.1.3";
 var csInterface = null, dsp = null;
 var fsModule = null, osModule = null, pathModule = null, execModule = null;
 var foundPresetPath = null, extensionPath = "", configPath = "";
