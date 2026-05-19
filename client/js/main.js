94e7f4c5d43546b91445066a5820d2f2c8417ada
ahmednajm1 <najmmediaa@gmail.com>
2026-05-19 20:51:29 +0400

    Deploying latest updates

--- client/js/main.js
+++ client/js/main.js
@@ -8,7 +8,7 @@ window.onerror = function(msg, url, line) {
     return true;
 };
 
-var CURRENT_VERSION = "1.2.0";
+var CURRENT_VERSION = "1.2.1";
 var csInterface = null, dsp = null;
 var fsModule = null, osModule = null, pathModule = null, execModule = null;
 var foundPresetPath = null, extensionPath = "", configPath = "";
